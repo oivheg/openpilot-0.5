ca8f14697eebb2a572713c3f50ef947bf9eaa198
oivheg <oivheg@gmail.com>
2018-07-14 19:19:05 +0200

    CHR Hybrid changes

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -117,6 +117,15 @@ class CarInterface(object):
       ret.mass = 3300./2.205 + std_cargo
       ret.steerKpV, ret.steerKiV = [[0.723], [0.0428]]
       ret.steerKf = 0.00006
+	  
+	  elif candidate == CAR.CHRH:
+      ret.safetyParam = 100
+      ret.wheelbase = 2.63906
+      ret.steerRatio = 13.6
+      tire_stiffness_factor = 0.7933
+      ret.mass = 3300./2.205 + std_cargo
+      ret.steerKpV, ret.steerKiV = [[0.723], [0.0428]]
+      ret.steerKf = 0.00006
 
 
     ret.steerRateCost = 1.
@@ -127,7 +136,7 @@ class CarInterface(object):
 
     # min speed to enable ACC. if car can do stop and go, then set enabling speed
     # to a negative value, so it won't matter.
-    if candidate in [CAR.PRIUS, CAR.RAV4H, CAR.LEXUS_RXH, CAR.CHR]: # rav4 hybrid can do stop and go
+    if candidate in [CAR.PRIUS, CAR.RAV4H, CAR.LEXUS_RXH, CAR.CHR, CAR.CHRH]: # rav4 hybrid can do stop and go
       ret.minEnableSpeed = -1.
     elif candidate in [CAR.RAV4, CAR.COROLLA]: # TODO: hack ICE to do stop and go
       ret.minEnableSpeed = 19. * CV.MPH_TO_MS
